45d31dee76ba394c98656a3b231d33603d827946
iliya zang <iliyazang@iliyas-mbp.mynetworksettings.com>
2022-09-21 16:08:13 -0400

    updates

--- vars.tf
+++ vars.tf
@@ -1,8 +1,8 @@
-variable "AWS_ACCESS_KEY" {
-}
+# variable "AWS_ACCESS_KEY" {
+# }
 
-variable "AWS_SECRET_KEY" {
-}
+# variable "AWS_SECRET_KEY" {
+# }
 
 variable "region" {
   default = "us-east-1"
