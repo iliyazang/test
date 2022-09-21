60251e560913b89e0f3427d23d76237014f5fdd0
iliya zang <iliyazang@iliyas-mbp.mynetworksettings.com>
2022-09-21 15:46:57 -0400

    made comments

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform { 
     backend "s3"{
         bucket="statefilebucket-iliya"
-        encrypt= true 
+        # encrypt= true 
         #dynamodb_table= "terraform-state-lock-dynamo"
         key= "terraform.tfstate" 
         region= "us-east-1"
