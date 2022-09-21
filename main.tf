ea73e3b5f02696732b5d5e4c10fab3b1a564285c
iliya zang <iliyazang@iliyas-mbp.mynetworksettings.com>
2022-09-21 14:40:55 -0400

    updated code

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ resource "aws_instance" "test-instance"{
     ami = "ami-05fa00d4c63e32376"
     instance_type = "t2.micro"
     associate_public_ip_address = true 
-    user_data=file("jenkins_install.sh")
+    user_data=<<EOF
+    echo >> #Welcome to a new dispensation
+
+    EOF
+    subnet_id=aws_subnet.public_subnet[0].id
+    security_groups=[aws_security_group.allow_ssh.id]
 
 }
